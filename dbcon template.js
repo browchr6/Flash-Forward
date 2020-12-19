21013fa674df24c4e7f9b751259d997c42ebea06
browchr6 <62980518+browchr6@users.noreply.github.com>
2020-12-18 20:49:51 -0800

    cleanup mysql

--- dbcon template.js
+++ dbcon template.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_browchr6',
-  password        : 'Winter2020',
-  database        : 'cs290_browchr6'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 
 module.exports.pool = pool;
