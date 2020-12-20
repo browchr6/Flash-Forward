222b392d18b929f2378cca805517a5bc27fcc6bb
browchr6 <62980518+browchr6@users.noreply.github.com>
2020-12-19 21:06:34 -0800

    edit & delete buttons
    added functionality

--- public/client_script_deck.js
+++ public/client_script_deck.js
@@ -13,7 +13,7 @@ function tableClick(event) {
     console.log('click registered');
     if (event.target.name === 'delete') {
         var xhr = new XMLHttpRequest();
-        xhr.open('delete','localhost:3000/',true);
+        xhr.open('DELETE','/',true);
         xhr.setRequestHeader('Content-Type','application/json');
         var payload = {};
         // go to parent node of event target, getElementsByTagName(input)
@@ -22,51 +22,86 @@ function tableClick(event) {
         current = current.parentNode; // this will be the row element
         var inputs = current.getElementsByTagName('input');
         // select id node and get value, store in payload.id
-        console.log(idNode);
         var idNode = inputs[0];
-        payload.id = idNode.id;
+        payload.id = idNode.value;
         console.log(payload)
         xhr.addEventListener('load',function(){
             if (xhr.status>=200 && xhr.status<400) {
                 var result = JSON.parse(xhr.responseText);
                 // check result
-                var tbody = current.parentNode;
-                tbody.removeChild(current);
+                if (result.deleteStatus==="success") {
+                    console.log('successful delete');
+                    var tbody = current.parentNode;
+                    tbody.removeChild(current);
+                    document.getElementById('cardCount').textContent--;
+                }
+                else {
+                    console.log("delete not successful");
+                }
             }
             else {
                 console.log('network error');
             }
         })
+        current.style.background = "grey";
         xhr.send(JSON.stringify(payload));
         event.preventDefault();
     }
     if (event.target.name ==='edit') {
-        var xhr = new XMLHttpRequest();
-        xhr.open('put','localhost:3000/',true);
-        xhr.setRequestHeader('Content-Type','application/json');
-        var payload = {};
-        var current = event.target;
-        current = current.parentNode;
-        current = current.parentNode;
-        current = current.parentNode;
-        var inputs = current.querySelectorAll('input');
-        for (let input of inputs) {
-            payload[input.name] = input.value;
+        if (event.target.value==='Edit') {
+            var current = event.target;
+            current = current.parentNode;
+            current = current.parentNode;
+            var inputs = current.querySelectorAll('input');
+            for (let input of inputs) {
+                input.removeAttribute('disabled');
+            }
+            event.target.setAttribute('value','Done');
+            event.target.style.backgroundColor = 'hsl(192, 96%, 48%)';
+            current.style.backgroundColor = "hsl(192, 0%, 89%)";
         }
-        console.log(payload)
-        xhr.addEventListener('load',function(){
-            if (xhr.status>=200 && xhr.status<400) {
-                var result = JSON.parse(xhr.responseText);
-                var appendNode = document.getElementById('deckTable');
-                appendNode = appendNode.lastElementChild; // tbody
-                appendNode.appendChild(createRow);
+        else if (event.target.value==='Done'){
+            var xhr = new XMLHttpRequest();
+            xhr.open('put','/',true);
+            xhr.setRequestHeader('Content-Type','application/json');
+            var payload = {};
+            var current = event.target;
+            current = current.parentNode;
+            current = current.parentNode;
+            var inputs = current.querySelectorAll('input');
+            for (let input of inputs) {
+                payload[input.name] = input.value;
             }
-            else {
-                console.log('network error');
-            }
-        })
-        xhr.send(JSON.stringify(payload));
-        event.preventDefault();
+            payload.deck = document.getElementById('deckTable').getAttribute('deckName');
+            console.log(payload)
+            xhr.addEventListener('load',function(){
+                if (xhr.status>=200 && xhr.status<400) {
+                    var result = JSON.parse(xhr.responseText);
+                    if (result.update==='success') {
+                        console.log('successful edit');
+                        /*
+                        var appendNode = document.getElementById('deckTable');
+                        appendNode = appendNode.lastElementChild; // tbody
+                        appendNode.appendChild(createRow);
+                        console.log('debug');
+                        */
+                        for (let input2 of inputs) {
+                            if (input2.getAttribute('type')!=='submit'){
+                                input2.setAttribute('disabled','true');
+                            }
+                        }
+                        event.target.setAttribute('value','Edit');
+                        event.target.style.background = "none";
+                        current.style.background = 'white';
+                    }
+                }
+                else {
+                    console.log('network error');
+                }
+            })
+            xhr.send(JSON.stringify(payload));
+            event.preventDefault();
+        }
     }
 }
 
